4b5d777cf045372c0aca5cfeeab0522e08b34251
Phat2763 <phat2763@gmail.com>
2026-02-12 14:13:38 +0700

    first upload

---
+++ src/main/java/com/valentine/HomeController.java
@@ -0,0 +1,13 @@
+package com.valentine;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // trả về index.html trong templates
+    }
+}
